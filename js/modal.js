67f4ef752a4be494e48d4999021d8eb7f0413292
juli7julia7 <juli7julia7@gmail.com>
2024-05-14 18:19:31 +0300

    update

--- js/modal.js
+++ js/modal.js
@@ -1,13 +1,20 @@
 const modalBtnOpen = document.querySelector('.modal-btn-open');
 const modalIsHidden = document.querySelector('.modal-is-hidden');
 const modalBtnClose = document.querySelector('.modal-btn-close');
+const formModalButton = document.querySelector('.form-modal__button-big');
 
 
-modalBtnOpen.addEventListener("click", () =>{
+modalBtnOpen.addEventListener("click", () => {
     modalIsHidden.classList.add('active');
 });
 
-modalBtnClose.addEventListener("click", ()=>{
+formModalButton.addEventListener("click", (e) => {
+    e.preventDefault();
+});
+formModalButton.addEventListener("click", ()=>{
+    modalIsHidden.classList.remove("active")
+})
+modalBtnClose.addEventListener("click", () => {
     modalIsHidden.classList.remove("active")
 });
 
